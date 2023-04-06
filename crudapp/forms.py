30001fc30ef71ee854e487904f8b953b239d90e9
Danial Rashid <sp20-bse-046@students.cuisahiwal.edu.pk>
2023-04-06 18:23:44 +0500

    new version

--- crudapp/forms.py
+++ crudapp/forms.py
@@ -1,9 +1,9 @@
 from django import forms
-from django.forms import inlineformset_factory
-
 from .models import *
 
 class EmployeeForm(forms.ModelForm):
     class Meta:
         model = registration
         fields = '__all__'
+        
+        
